3ed7127ec2e3d32b996c42d8b51fb8906cf98641
MahBoiDeveloper <mah.boi.developer@gmail.com>
2025-08-27 18:40:06 +0300

    Add test type

--- src/IniSerializerTests/Program.cs
+++ src/IniSerializerTests/Program.cs
@@ -6,6 +6,8 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        TestClass tc = new TestClass();
+        var tmp = IniSerializer.Serialize<TestClass>(tc);
         Console.WriteLine("Hello, World!");
     }
 }
